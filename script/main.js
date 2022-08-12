db17e3ac5e1d301918e8b07c2aa20a222f26ac08
Ruslan Yarosh <ruslan.yarosh.96@gmail.com>
2022-08-12 14:50:13 +0300

    add js code for open/hide menu on click

---
+++ script/main.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector('.main-nav');
+const menuBtn = document.querySelector('.tablet-menu');
+const screenWitdh = window.screen.width;
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'block';
+    document.querySelector
+});
+
+
+if(screenWitdh <= 780)
+menu.addEventListener('click', () => {
+    menu.style.display = 'none';
+})
+
