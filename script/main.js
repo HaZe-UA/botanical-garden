c68829a38b6190591e072b617acd8eae89383706
HaZe-UA <ruslan.yarosh.96@gmail.com>
2022-08-19 00:14:02 +0300

    reworked code

--- script/main.js
+++ script/main.js
@@ -1,15 +1,15 @@
 const menu = document.querySelector('.main-nav');
-const menuBtn = document.querySelector('.tablet-menu');
+const menuBtn = document.querySelector('.mobile-menu');
 const screenWitdh = window.screen.width;
 
 menuBtn.addEventListener('click', () => {
     menu.style.display = 'block';
-    document.querySelector
 });
 
 
-if(screenWitdh <= 780)
-menu.addEventListener('click', () => {
-    menu.style.display = 'none';
-})
+if(screenWitdh <= 780) {
+    menu.addEventListener('click', () => {
+        menu.style.display = 'none';
+    })
+};
 
